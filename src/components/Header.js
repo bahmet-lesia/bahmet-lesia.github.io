9d5cf427ca7481d6ef52c96c69e94b3a45baab37
Lesia <93528257+bahmet-lesia@users.noreply.github.com>
2023-06-26 10:51:33 +0300

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import { Toolbar } from "@mui/material";
+import Typography from "@mui/material/Typography";
+
+function Header() {
+  return (
+    <AppBar sx={{ flexDirection: "row" }} color="transparent" position="fixed">
+      <Toolbar>
+        <Typography variant="h6" color="#00C4C4">
+          LOGO
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
